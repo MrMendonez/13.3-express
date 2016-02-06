e73b6d4f98826fb3ab43aa82d4e3f5ff8efb617e
MrMendonez <dmendonez@gmail.com>
2016-02-06 10:32:52 -0500

    init commit

---
+++ server.js
@@ -0,0 +1,11 @@
+// Create a 3 page website
+// Home
+// Register
+// Dashboard
+// Include jQuery (not from CDN)
+// Include Bootstrap (not from CDN)
+// Create a navbar
+// In the navbar, have a login link. Clicking login should open a bootstrap modal that has an email and password field (and button)
+// Make this look good!
+// On the Registration form, use JavaScript / jQuery to ensure all fields are filled out before POSTing the form
+
